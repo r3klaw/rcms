7f292665f93337e247ae03193cd5cad57af9dea2
FlutLab.io <>
2021-02-03 06:59:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() => runApp(MyApp());
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'bdm self service app';
 
   @override
   Widget build(BuildContext context) {
