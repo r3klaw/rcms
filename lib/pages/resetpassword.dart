38d0c26bcc59701b9e1f78ac2fb03857137bcdc7
FlutLab.io <>
2021-02-09 23:12:12 +0000

    Autosave

--- lib/pages/resetpassword.dart
+++ lib/pages/resetpassword.dart
@@ -1,19 +1,67 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class Reset extends StatefulWidget {
-//   @override
-//   _ResetState createState() => _ResetState();
-// }
+class ResetPassword extends StatefulWidget {
+  @override
+  _ResetPasswordState createState() => _ResetPasswordState();
+}
 
-// class _ResetState extends State<Reset> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Bdm Self Service App'),
-//         centerTitle: true,
-//         backgroundColor: Colors.blue
-//       ),
-//     );
-//   }
-// }
+class _ResetPasswordState extends State<ResetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bdm Self Service App'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/rcms.png'),
+                radius: 60.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Enter Username or Email'),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Enter Password'),
+            ),
+            SizedBox(height: 20.0),
+            FlatButton(
+              onPressed: () {},
+              child: Text('login'),
+              color: Colors.green,
+              minWidth: 300.0,
+            ),
+            SizedBox(height: 20.0),
+            Text('Forgot Password?'),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Reset Password'),
+              color: Colors.green,
+              minWidth: 200.0,
+            ),
+            SizedBox(height: 20.0),
+            Text('Do not have an account yet?'),
+            FlatButton(
+              onPressed: () {},
+              child: Text('SignUp'),
+              color: Colors.green,
+              minWidth: 200.0,
+            ),
+          ],
+        ),
+        // ],
+      ),
+      // ),
+    );
+  }
+}
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -12,42 +12,60 @@ class _SignupState extends State<Signup> {
       appBar: AppBar(
         title: const Text('Bdm Self Service App'),
         centerTitle: true,
-        backgroundColor: Colors.blue
+        backgroundColor: Colors.blue,
+        elevation: 0.0,
       ),
-      body: Container(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            // child: CircleAvatar(
-            //         backgroundImage: AssetImage('assets/rcms.png'),
-            //         radius: 40.0,
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/rcms.png'),
+                radius: 60.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
             TextFormField(
-                    decoration: InputDecoration(
-                    labelText: 'Enter your fullname'
-                    ),
-                    ),
-                    TextFormField(
-                    decoration: InputDecoration(
-                    labelText: 'Enter your email address'
-                    ),
-                    ),
-                    TextFormField(
-                    decoration: InputDecoration(
-                    labelText: 'Enter your Phone number'
-                    ),
-                    ),
-                    TextFormField(
-                    decoration: InputDecoration(
-                    labelText: 'Create password'
-                    ),
-                    ),
-                    TextFormField(
-                    decoration: InputDecoration(
-                    labelText: 'Confirm Password'
-                    ),
-                    ),
+              decoration: InputDecoration(labelText: 'Enter Full Name'),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Enter Email Address'),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Enter Phone Number'),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Create Password'),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+            ),
+            SizedBox(height: 20.0),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Sign Up'),
+              color: Colors.green,
+              minWidth: 300.0,
+            ),
+            SizedBox(height: 20.0),
+            Text('Already have an account?'),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Sign In'),
+              color: Colors.green,
+              minWidth: 200.0,
+            ),
           ],
         ),
+        // ],
       ),
+      // ),
     );
   }
 }
