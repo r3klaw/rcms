2ab9e2ef1d09cddfe76160c5253888dec02d54b5
FlutLab.io <>
2021-02-09 22:11:17 +0000

    Autosave

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -12,13 +12,84 @@ class _SigninState extends State<Signin> {
       appBar: AppBar(
         title: const Text('Bdm Self Service App'),
         centerTitle: true,
-        backgroundColor: Colors.blue
+        backgroundColor: Colors.blue,
         elevation: 0.0,
       ),
-      body: CircleAvatar(
-      backgroundImage: AssetImage('rcms.png'),
-      radius: 40.0,
-      ),
+       body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget> [
+                Center(
+                    child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/rcms.png'),
+                    radius: 40.0,
+                ),
+                ),
+                Divider(
+                    height: 90.0,
+                    color: Colors.grey[800],
+                ),
+                Text(
+                    'NAME',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                    ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                    'David Rashid',
+                    style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 2.0,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                    ),
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                    'CURRENT NINJA LEVEL',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        letterSpacing: 2.0,
+                    ),
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                    '8',
+                    style: TextStyle(
+                        color: Colors.amberAccent[200],
+                        letterSpacing: 2.0,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                    ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                    children: <Widget>[
+                        Icon(
+                            Icons.mail,
+                            color: Colors.grey[400],
+                        ),
+                        SizedBox(width: 10.0),
+                        Text(
+                            'rashid.david12@gmail.com',
+                            style: TextStyle(
+                                color: Colors.grey[400],
+                                fontSize: 18.0,
+                                letterSpacing: 1.0,
+                            ),
+                        ),
+                    ],
+                ),
+            ],
+        ),
+    ),
+    );
+  }
+}
+
       // body: Container(
       //   child: Column(
       //     children: <Widget>[
