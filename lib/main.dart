7e5c12a51b4660e43b7f50ea64af70d7182fedc8
FlutLab.io <>
2021-02-09 23:13:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:rcms/pages/resetpassword.dart';
 void main() => runApp(MaterialApp(
   // home: Home(),
   routes: {
-    '/': (context) => Signin(),
+    '/': (context) => Signup(),
     '/home': (context) => Home(),
     '/signup': (context) => Signup(),
     '/signin': (context) => Signin(),
