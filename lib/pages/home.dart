6e9528772fb3a925e91167c0fcfb465446cdc43e
FlutLab.io <>
2021-02-10 08:06:29 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -26,7 +26,8 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
+  // static const List<Widget> _widgetOptions = <Widget>[
+    List<Widget> _widgetOptions = <Widget>[
     Text(
       'Index 0: Home',
       style: optionStyle,
