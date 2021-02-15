615a767ee7219e8fdeb673eb07f6b3156878c133
FlutLab.io <>
2021-02-15 18:31:10 +0000

    Autosave

--- lib/pages/businessrenew.dart
+++ lib/pages/businessrenew.dart
@@ -38,7 +38,7 @@ class _BusinessRenewState extends State<BusinessRenew> {
               },
               child: Text('Cancel'),
               color: Colors.green,
-              minWidth: 150.0,
+              minWidth: 100.0,
             ),
       SizedBox(width: 50),
       FlatButton(
@@ -50,7 +50,7 @@ class _BusinessRenewState extends State<BusinessRenew> {
               },
               child: Text('Continue'),
               color: Colors.green,
-              minWidth: 150.0,
+              minWidth: 100.0,
             ),
             ],
             ),
