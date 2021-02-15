657e10bf3bd533cbd1152f05258c046a179ba371
FlutLab.io <>
2021-02-15 21:18:49 +0000

    Autosave

--- lib/pages/businessinstruction.dart
+++ lib/pages/businessinstruction.dart
@@ -24,38 +24,7 @@ class BusinessInstruction extends StatelessWidget {
             Text('A pop-up message will apear prompting you to confirm payment of renewal  fee of business number business_no1, accept the prompt and enter your EVC pin to complete the transaction. using E-wallet, ### ussd will be diducted from your account.'),
             SizedBox(height: 20.0),
             Text('After successful payment, go to home and click Receipts to print digital Receipt or my bills to check status of all your bills'),
-            bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.blue
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.payment),
-            label: 'Bills',
-            backgroundColor: Colors.blue
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Assets',
-            backgroundColor: Colors.blue
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-            backgroundColor: Colors.blue
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'User',
-            backgroundColor: Colors.blue
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+            
           ],
         ),
       ),
