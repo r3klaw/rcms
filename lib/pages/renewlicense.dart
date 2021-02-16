c4271fba7e054735dc9232d2e854711653f678f6
FlutLab.io <>
2021-02-16 21:03:31 +0000

    Autosave

--- lib/pages/renewlicense.dart
+++ lib/pages/renewlicense.dart
@@ -22,22 +22,10 @@ class _RenewLicenseState extends State<RenewLicense> {
         backgroundColor: Colors.blue,
         elevation: 0.0,
       ),
-      appBar: AppBar(
-        leading: Icon(Icons.menu),
-        title: const Text('Bdm Self Service App'),
-        centerTitle: true,
-        actions: [
-      Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Icon(Icons.search),
-      ),
-      ],
-        backgroundColor: Colors.blue,
-        elevation: 0.0,
-      ),
       body: ListView(
   padding: const EdgeInsets.all(8),
   children: <Widget>[
+    Text('Renew License')
           ListTile(
             title: Text('Business_no1'),
             trailing: Icon(Icons.keyboard_arrow_right),
