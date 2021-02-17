ea43e56556f2e10bd0818ef5bb1e3435c3e4e216
FlutLab.io <>
2021-02-17 12:00:58 +0000

    Autosave

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,6 +19,32 @@ class _HomeState extends State<Home> {
     MyAssets(),
     History(),
     Profile(),
+    PopupMenuButton(
+  icon: Icon(Icons.more_vert),
+  itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+    const PopupMenuItem(
+      child: ListTile(
+        leading: Icon(Icons.add),
+        title: Text('Item 1'),
+      ),
+    ),
+    const PopupMenuItem(
+      child: ListTile(
+        leading: Icon(Icons.anchor),
+        title: Text('Item 2'),
+      ),
+    ),
+    const PopupMenuItem(
+      child: ListTile(
+        leading: Icon(Icons.article),
+        title: Text('Item 3'),
+      ),
+    ),
+    const PopupMenuDivider(),
+    const PopupMenuItem(child: Text('Item A')),
+    const PopupMenuItem(child: Text('Item B')),
+  ],
+),
   ];
 
   @override
