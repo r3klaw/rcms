a0327ecd320573f9cdad51c91ca76fbbb0a864f7
FlutLab.io <>
2021-02-17 11:29:03 +0000

    Autosave

--- lib/pages/services.dart
+++ lib/pages/services.dart
@@ -22,33 +22,35 @@ class _ServicesState extends State<Services> {
         backgroundColor: Colors.blue,
         elevation: 0.0,
       ),
-      body: ListView(
-  // This next line does the trick.
-  scrollDirection: Axis.horizontal,
-  children: <Widget>[
-    Container(
-      width: 160.0,
-      height: 50.0,
-      color: Colors.red,
-    ),
-    Container(
-      width: 160.0,
-      color: Colors.blue,
-    ),
-    Container(
-      width: 160.0,
-      color: Colors.green,
-    ),
-    Container(
-      width: 160.0,
-      color: Colors.yellow,
-    ),
-    Container(
-      width: 160.0,
-      color: Colors.orange,
-    ),
-  ],
-)
+       body: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ),
     );
   }
 }
