29bdedc30d918db73b5d9088257547458b18226f
FlutLab.io <>
2021-02-17 10:18:18 +0000

    Autosave

--- lib/pages/services.dart
+++ lib/pages/services.dart
@@ -29,57 +29,15 @@ class _ServicesState extends State<Services> {
     children: ListTile.divideTiles( //          <-- ListTile.divideTiles
       context: context,
       tiles: [
-    Text('Renew License',
-    textAlign: TextAlign.center,
-    style: TextStyle(fontSize: 16),
-    ),
-    Text('Browse through your lincense and renew',
-    textAlign: TextAlign.center,
-    ),
-          ListTile(
-            title: Text('Business_no1'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              Navigator.pushNamed(context, '/businessdetails');
-            },
+    ListTile(
+            title: Text('Sun'),
           ),
           ListTile(
-            title: Text('Business_no2'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              Navigator.pushNamed(context, '/businessdetails');
-            },
+            title: Text('Moon'),
           ),
           ListTile(
-            title: Text('Business_no3'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              Navigator.pushNamed(context, '/businessdetails');
-            },
+            title: Text('Star'),
           ),
-          ListTile(
-            title: Text('Business_no4'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              Navigator.pushNamed(context, '/businessdetails');
-            },
-          ),
-          ListTile(
-            title: Text('Business_no5'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              Navigator.pushNamed(context, '/businessdetails');
-            },
-          ),
-          ListTile(
-            title: Text('Business_no6'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () {
-              Navigator.pushNamed(context, '/businessdetails');
-            },
-          ),
-    
-    
     
   ],
 ).toList(),
