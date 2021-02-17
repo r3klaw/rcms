0bb5c87983d56b7dbe0ef1d97f723c0b28afdf5f
FlutLab.io <>
2021-02-17 11:29:59 +0000

    Autosave

--- lib/pages/services.dart
+++ lib/pages/services.dart
@@ -50,6 +50,31 @@ class _ServicesState extends State<Services> {
               ),
             ],
           ),
+          ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              ),
+            ],
+          ),
         ),
     );
   }
