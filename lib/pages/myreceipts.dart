b7d7060542b2b765cbe120dd78ab62f9b89b68c0
FlutLab.io <>
2021-02-18 08:15:47 +0000

    Autosave

--- lib/pages/myreceipts.dart
+++ lib/pages/myreceipts.dart
@@ -31,7 +31,7 @@ class _MyReceiptsState extends State<MyReceipts> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             
-            Text('My Receipts'),
+           header: Text('My Receipts'),
             SizedBox(height: 10.0),
             Text('Search for your receipts using unique identifier, e.g House number, Vehicle registration number'),
               SizedBox(height: 10.0),
