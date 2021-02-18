7735e63d49c54539349250e2b09dda36a8f14323
FlutLab.io <>
2021-02-18 04:52:32 +0000

    Autosave

--- lib/pages/myreceipts.dart
+++ lib/pages/myreceipts.dart
@@ -1,44 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyReceipts extends StatefulWidget {
-  @override
-  _MyReceiptsState createState() => _MyReceiptsState();
-}
-
-class _MyReceiptsState extends State<MyReceipts> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // leading: Icon(Icons.menu),
-        title: const Text('Bdm Self Service App'),
-        centerTitle: true,
-        actions: [
-      Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Icon(Icons.search),
-      ),
-      ],
-        backgroundColor: Colors.blue,
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            
-            Text('My Receipts'),
-            SizedBox(height: 10.0),
-            Text('Search for your receipts using unique identifier, e.g House number, Vehicle registration number'),
-            // SizedBox(height: 20.0),
-            
-          ],
-        ),
-      ),
-    );
-  }
-  class DataTableDemo extends StatelessWidget {
+class DataTableDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -129,4 +91,43 @@ class _DataSource extends DataTableSource {
   @override
   int get selectedRowCount => _selectedCount;
 }
+
+class MyReceipts extends StatefulWidget {
+  @override
+  _MyReceiptsState createState() => _MyReceiptsState();
+}
+
+class _MyReceiptsState extends State<MyReceipts> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // leading: Icon(Icons.menu),
+        title: const Text('Bdm Self Service App'),
+        centerTitle: true,
+        actions: [
+      Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Icon(Icons.search),
+      ),
+      ],
+        backgroundColor: Colors.blue,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            
+            Text('My Receipts'),
+            SizedBox(height: 10.0),
+            Text('Search for your receipts using unique identifier, e.g House number, Vehicle registration number'),
+            // SizedBox(height: 20.0),
+            
+          ],
+        ),
+      ),
+    );
+  }
 }
