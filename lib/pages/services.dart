06657a34d36d1223f69d523b42a71c07c6605c7b
FlutLab.io <>
2021-02-22 09:52:55 +0000

    Autosave

--- lib/pages/services.dart
+++ lib/pages/services.dart
@@ -25,7 +25,67 @@ class _ServicesState extends State<Services> {
        body: Row(
   children: <Widget>[
     Expanded(
-      child: Text('Deliver features faster', textAlign: TextAlign.center),
+      child: ListView(
+  padding: const EdgeInsets.all(8),
+  // children: <Widget>[
+    children: ListTile.divideTiles( //          <-- ListTile.divideTiles
+      context: context,
+      tiles: [
+    Text('Renew License',
+    textAlign: TextAlign.center,
+    style: TextStyle(fontSize: 16),
+    ),
+    Text('Browse through your lincense and renew',
+    textAlign: TextAlign.center,
+    ),
+          ListTile(
+            title: Text('Business_no1'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/businessdetails');
+            },
+          ),
+          ListTile(
+            title: Text('Business_no2'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/businessdetails');
+            },
+          ),
+          ListTile(
+            title: Text('Business_no3'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/businessdetails');
+            },
+          ),
+          ListTile(
+            title: Text('Business_no4'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/businessdetails');
+            },
+          ),
+          ListTile(
+            title: Text('Business_no5'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/businessdetails');
+            },
+          ),
+          ListTile(
+            title: Text('Business_no6'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/businessdetails');
+            },
+          ),
+    
+    
+    
+  ],
+).toList(),
+),
     ),
     Expanded(
       child: Text('Craft beautiful UIs', textAlign: TextAlign.center),
