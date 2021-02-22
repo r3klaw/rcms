c818cc59f90921a475dc30c1a402ab8976e17a58
FlutLab.io <>
2021-02-22 08:11:28 +0000

    Autosave

--- lib/pages/myassets.dart
+++ lib/pages/myassets.dart
@@ -51,7 +51,7 @@ class _MyAssetsState extends State<MyAssets> {
                 Tab(icon: Icon(Icons.directions_bike)),
               ],
             ),
-            title: Text('Tabs Demo'),
+            // title: Text('Tabs Demo'),
           ),
           TabBarView(
             children: [
