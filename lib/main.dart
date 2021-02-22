8b3ee9d18abf1b2551151e0fc5134bd7d8a4b69b
FlutLab.io <>
2021-02-22 09:30:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ void main() => runApp(MaterialApp(
   // home: Home(),
   // initialRoute: '/signin',
   routes: {
-    '/': (context) => History(),
+    '/': (context) => Home(),
     '/home': (context) => Home(),
     '/signup': (context) => Signup(),
     '/signin': (context) => Signin(),
