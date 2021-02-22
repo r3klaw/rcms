cfb514d3e10adebab82a748e2e41825f55a7efac
FlutLab.io <>
2021-02-22 08:49:35 +0000

    Autosave

--- lib/pages/myassets.dart
+++ lib/pages/myassets.dart
@@ -21,6 +21,13 @@ class _MyAssetsState extends State<MyAssets> {
       ],
         backgroundColor: Colors.blue,
         elevation: 0.0,
+        TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -45,13 +52,7 @@ class _MyAssetsState extends State<MyAssets> {
             ),
             
 
-              TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
+              
             TabBarView(
             children: [
               Icon(Icons.directions_car),
