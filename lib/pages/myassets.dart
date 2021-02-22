fb06c06653eb8634bcbd177594b2850bb41bbd2c
FlutLab.io <>
2021-02-22 08:46:49 +0000

    Autosave

--- lib/pages/myassets.dart
+++ lib/pages/myassets.dart
@@ -8,61 +8,86 @@ class MyAssets extends StatefulWidget {
 class _MyAssetsState extends State<MyAssets> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // leading: Icon(Icons.menu),
-        title: const Text('Bdm Self Service App'),
-        centerTitle: true,
-        actions: [
-      Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Icon(Icons.search),
-      ),
-      ],
-        backgroundColor: Colors.blue,
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
+//     return Scaffold(
+//       appBar: AppBar(
+//         // leading: Icon(Icons.menu),
+//         title: const Text('Bdm Self Service App'),
+//         centerTitle: true,
+//         actions: [
+//       Padding(
+//       padding: EdgeInsets.symmetric(horizontal: 16),
+//       child: Icon(Icons.search),
+//       ),
+//       ],
+//         backgroundColor: Colors.blue,
+//         elevation: 0.0,
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+//         child: Column(
           
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
             
-            Text('My Assets'),
-            SizedBox(height: 10.0),
-            Text('Check all your assets'),
-              SizedBox(height: 10.0),
-              TextField (  
-              decoration: InputDecoration(  
-              border: OutlineInputBorder(
-                borderRadius:BorderRadius.circular(5.0)
-              ),
-              labelText: 'Search',  
-              prefixIcon: Icon(Icons.search),
-              hintText: 'Search using unique identifier e.g House No',  
-              ),  
-            ),
+//             Text('My Assets'),
+//             SizedBox(height: 10.0),
+//             Text('Check all your assets'),
+//               SizedBox(height: 10.0),
+//               TextField (  
+//               decoration: InputDecoration(  
+//               border: OutlineInputBorder(
+//                 borderRadius:BorderRadius.circular(5.0)
+//               ),
+//               labelText: 'Search',  
+//               prefixIcon: Icon(Icons.search),
+//               hintText: 'Search using unique identifier e.g House No',  
+//               ),  
+//             ),
             
 
-              TabBar(
+//               TabBar(
+//               tabs: [
+//                 Tab(icon: Icon(Icons.directions_car)),
+//                 Tab(icon: Icon(Icons.directions_transit)),
+//                 Tab(icon: Icon(Icons.directions_bike)),
+//               ],
+//             ),
+//             TabBarView(
+//             children: [
+//               Icon(Icons.directions_car),
+//               Icon(Icons.directions_transit),
+//               Icon(Icons.directions_bike),
+//             ],
+//           ),
+
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+Scaffold(
+          appBar: AppBar(
+            title: Text('Fixed Tabs'),
+            automaticallyImplyLeading: false,
+            backgroundColor: Color(0xff5808e5),
+            bottom: TabBar(
+              indicatorColor: Colors.white,
               tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(text: 'DOGS', icon: Icon(Icons.favorite)),
+                Tab(text: 'CATS', icon: Icon(Icons.music_note)),
+                Tab(text: 'BIRDS', icon: Icon(Icons.search)),
               ],
             ),
-            TabBarView(
+          ),
+          body: TabBarView(
             children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
+                Center(child: Text('DOGS')),
+                Center(child: Text('CATS')),
+                Center(child: Text('BIRDS')),
             ],
           ),
-
-          ],
         ),
       ),
     );
-  }
-}
