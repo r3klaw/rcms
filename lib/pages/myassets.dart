362438590c2e5d46e60a12082968c89a4546c301
FlutLab.io <>
2021-02-22 09:06:53 +0000

    Autosave

--- lib/pages/myassets.dart
+++ lib/pages/myassets.dart
@@ -45,19 +45,51 @@ class _MyAssetsState extends State<MyAssets> {
             ),
             
 
-              TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-            TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
+               DefaultTabController(
+            length: 4, // length of tabs
+            initialIndex: 0,
+            child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+              Container(
+                child: TabBar(
+                  labelColor: Colors.green,
+                  unselectedLabelColor: Colors.black,
+                  tabs: [
+                    Tab(text: 'Tab 1'),
+                    Tab(text: 'Tab 2'),
+                    Tab(text: 'Tab 3'),
+                    Tab(text: 'Tab 4'),
+                  ],
+                ),
+              ),
+              Container(
+                height: 400, //height of TabBarView
+                decoration: BoxDecoration(
+                  border: Border(top: BorderSide(color: Colors.grey, width: 0.5))
+                ),
+                child: TabBarView(children: <Widget>[
+                  Container(
+                    child: Center(
+                      child: Text('Display Tab 1', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('Display Tab 2', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('Display Tab 3', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: Text('Display Tab 4', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ])
+              )
+            ])
           ),
 
           ],
