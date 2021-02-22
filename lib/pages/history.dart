a62ab760c5e52ebb2ff327b72d4ce00d3c9e1249
FlutLab.io <>
2021-02-22 09:28:42 +0000

    Autosave

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -50,7 +50,7 @@ class _HistoryState extends State<History> {
             child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
               Container(
                 child: TabBar(
-                  labelColor: Colors.Blue,
+                  labelColor: Colors.blue,
                   unselectedLabelColor: Colors.black,
                   tabs: [
                     Tab(text: 'All'),
