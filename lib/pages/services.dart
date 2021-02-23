7715da8a1db50502a42d51a44b3a232e1b69280a
FlutLab.io <>
2021-02-23 21:29:09 +0000

    Autosave

--- lib/pages/services.dart
+++ lib/pages/services.dart
@@ -82,6 +82,23 @@ class _ServicesState extends State<Services> {
     ),
   ],
 ),
+GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            _customCard(
+              imageUrl: image1, item: item1, price: price1, count: count1
+            ),
+            _customCard(
+              imageUrl: image2, item: item2, price: price2, count: count2
+            ),
+            _customCard(
+              imageUrl: image3, item: item3, price: price3, count: count3
+            ),
+            _customCard(
+              imageUrl: image4, item: item4, price: price4, count: count4
+            ),
+          ],
+        ),
 
       //  Container(
       //     margin: EdgeInsets.symmetric(vertical: 20.0),
