8c8105d92529ff3a70afa2c28de97885341a15e8
FlutLab.io <>
2021-02-24 08:09:45 +0000

    Autosave

--- lib/pages/services.dart
+++ lib/pages/services.dart
@@ -32,7 +32,7 @@ class _ServicesState extends State<Services> {
               children: [
                 Card(
                   color: Colors.teal,
-                child: Image.asset('assets/rcmsbg.png'),
+                children: Image.asset('assets/rcmsbg.png'),
                   child: FlatButton(
               shape: RoundedRectangleBorder(
               // borderRadius: BorderRadius.circular(18.0),
