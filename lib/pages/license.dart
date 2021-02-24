4cd7b9516488de42fc4ea7bebf42579ec8d5298a
FlutLab.io <>
2021-02-24 07:49:27 +0000

    Autosave

--- lib/pages/license.dart
+++ lib/pages/license.dart
@@ -22,7 +22,57 @@ class _LicenseState extends State<License> {
         backgroundColor: Colors.blue,
         elevation: 0.0,
       ),
-       body: 
+       body: Container(
+            // height: 200,
+            child: GridView.count(
+              scrollDirection: Axis.vertical,
+              crossAxisCount: 3,
+              children: [
+                Card(
+                  color: Colors.teal,
+                  child: FlatButton(
+              shape: RoundedRectangleBorder(
+              // borderRadius: BorderRadius.circular(18.0),
+              side: BorderSide(color: Colors.white)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/renewlicense');
+              },
+              child: Text('Renew License'),
+              color: Colors.teal,
+              minWidth: 120.0,
+            ),
+                ),
+                Card(
+                  color: Colors.cyan,
+                  child: FlatButton(
+              shape: RoundedRectangleBorder(
+              // borderRadius: BorderRadius.circular(18.0),
+              side: BorderSide(color: Colors.white)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/renewlicense');
+              },
+              child: Text('Print License'),
+              color: Colors.cyan,
+              minWidth: 120.0,
+            ),
+                ),
+                Card(
+                  color: Colors.yellowAccent,
+                  child: FlatButton(
+              shape: RoundedRectangleBorder(
+              // borderRadius: BorderRadius.circular(18.0),
+              side: BorderSide(color: Colors.white)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/myreceipts');
+              },
+              child: Text('Receipts'),
+              color: Colors.yellowAccent,
+              minWidth: 120.0,
+            ),
+                ), 
+              ],
+            ),
+          ),
        Row(
   children: <Widget>[
     Expanded(
